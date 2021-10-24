ab3a0d872757ac77f124decaf64f86919db6b94d
Jgangl <30756487+Jgangl@users.noreply.github.com>
2021-10-24 18:27:30 -0400

    Testing portals

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -6,6 +6,10 @@ public class Portal : MonoBehaviour
 {
     public Portal connectedPortal;
 
+    private bool canTeleport = true;
+
+    private Vector3 playerVelocityOnEnter;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +23,37 @@ public class Portal : MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
-        
+        PlayerController player = collision.GetComponent<PlayerController>();
+        if (player != null) {
+            TeleportPlayer(player);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        PlayerController player = collision.GetComponent<PlayerController>();
+        if (player != null) {
+            SetCanTeleport(true);
+        }
+    }
+
+    private void TeleportPlayer(PlayerController player) {
+        Debug.Log("Teleported player");
+        if (connectedPortal != null && canTeleport) {
+            connectedPortal.SetCanTeleport(false);
+
+            Rigidbody2D playerRb = player.GetComponent<Rigidbody2D>();
+            playerVelocityOnEnter = playerRb.velocity;
+
+            float speed = playerVelocityOnEnter.magnitude;
+            Vector3 newDirection = connectedPortal.transform.up;
+
+            player.transform.position = connectedPortal.transform.position;
+            playerRb.velocity = newDirection * speed;
+        }
+
+    }
+
+    private void SetCanTeleport(bool canTeleport) {
+        this.canTeleport = canTeleport;
     }
 }
