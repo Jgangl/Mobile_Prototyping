93bc780d4f515e2e263c638a7802e6e342405045
Jgangl <joshua.gangl@gmail.com>
2022-11-23 20:52:07 -0500

    Using shapes instead of vectrosity for drawing of trajectory

--- Assets/_Project/Scripts/BasicTrajectory.cs
+++ Assets/_Project/Scripts/BasicTrajectory.cs
@@ -3,8 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Vectrosity;
+using Shapes;
 
-public class BasicTrajectory : Singleton<BasicTrajectory>
+public class BasicTrajectory : ImmediateModeShapeDrawer
 {
     
     //[SerializeField] float maxDuration = 1f;
@@ -18,6 +19,7 @@ public class BasicTrajectory : Singleton<BasicTrajectory>
     
     LineRenderer lineRenderer;
     VectorLine trajectoryVisualLine;
+    private PolylinePath polyPath;
     
     private List<float> lineWidths;
 
@@ -25,12 +27,15 @@ public class BasicTrajectory : Singleton<BasicTrajectory>
     [SerializeField] private float gravityScale = 2f;
     [SerializeField] private LayerMask collisionLayers;
 
+    //[SerializeField] private Polyline polyline;
+
     private List<Vector2> linePositions;
 
     private Camera mainCam;
     
     private void Awake()
     {
+        polyPath = new PolylinePath();
         linePositions = new List<Vector2>();
         mainCam = Camera.main;
         //Debug.Log("Basic Trajectory awake");
@@ -48,15 +53,18 @@ public class BasicTrajectory : Singleton<BasicTrajectory>
             lineWidths[i] = Mathf.Clamp(lineWidthStart - (i * stepSize), lineWidthEnd, lineWidthStart);
         }
         
-        trajectoryVisualLine = new VectorLine("Trajectory", new List<Vector2>(), lineTex, lineWidthStart, lineType);
+        //trajectoryVisualLine = new VectorLine("Trajectory", new List<Vector2>(), lineTex, lineWidthStart, lineType);
         
-        trajectoryVisualLine.color = lineColor;
-        trajectoryVisualLine.textureScale = 1.0f;
-        trajectoryVisualLine.smoothWidth = true;
+        //trajectoryVisualLine.color = lineColor;
+        //trajectoryVisualLine.textureScale = 1.0f;
+        //trajectoryVisualLine.smoothWidth = true;
     }
 
     public void SimulateArc(Vector2 launchPosition, Vector2 directionVector, float velocity, float mass)
     {
+        polyPath.ClearAllPoints();
+        PolylinePoint polylinePoint = new PolylinePoint(launchPosition, Color.green, 1f);
+        polyPath.AddPoint(polylinePoint);
         linePositions.Clear();
         linePositions.Add(launchPosition);
         float initialVelocity = velocity / mass * Time.fixedDeltaTime;
@@ -69,30 +77,58 @@ public class BasicTrajectory : Singleton<BasicTrajectory>
             Vector2 previousPos = linePositions[i - 1];
             Vector2 rayDirection = calculatedPosition - previousPos;
             
+            PolylinePoint point = new PolylinePoint(calculatedPosition, Color.green, 1f);
+            
             RaycastHit2D hit = Physics2D.Raycast(previousPos, rayDirection, rayDirection.magnitude, collisionLayers);
             if (hit.collider)
             {
                 // Collided with something
+                
+                point = new PolylinePoint(hit.point, Color.green, 1f);
+                linePositions.Add(hit.point);
+                polyPath.AddPoint(point);
+                
                 break;
             }
             
-            Vector2 uiPoint = mainCam.WorldToScreenPoint(calculatedPosition);
-            trajectoryVisualLine.points2.Add(uiPoint);
+            //Vector2 uiPoint = mainCam.WorldToScreenPoint(calculatedPosition);
+            //trajectoryVisualLine.points2.Add(uiPoint);
 
-
-
+            
+            //polyline.points.Add(point);
+            polyPath.AddPoint(point);
+            
             linePositions.Add(calculatedPosition);
         }
-        
+
         //trajectoryVisualLine.SetWidths(lineWidths);
         //trajectoryVisualLine.Draw();
-        trajectoryVisualLine.points2.Clear();
+        //polyline.points.Clear();
+        //trajectoryVisualLine.points2.Clear();
     }
 
     public void ClearArc()
     {
+        polyPath.ClearAllPoints();
+        //polyline.points.Clear();
         linePositions.Clear();
-        trajectoryVisualLine.points2.Clear();
+        //trajectoryVisualLine.points2.Clear();
         //trajectoryVisualLine.Draw();
+    }
+
+    public override void DrawShapes(Camera cam)
+    {
+        using( Draw.Command( cam ) ){
+
+            // set up static parameters. these are used for all following Draw.Line calls
+            Draw.LineGeometry = LineGeometry.Flat2D;
+            Draw.ThicknessSpace = ThicknessSpace.Pixels;
+            Draw.Thickness = 4; // 4px wide
+
+            Draw.Polyline(polyPath,  false, 5f, Color.green);
+            
+            if (linePositions.Count > 0)
+                Draw.Disc(linePositions[^1], Vector3.forward, 0.1f, DiscColors.Flat(Color.green));
+        }
     }
 }
--- Assets/_Project/Scripts/Testing.cs
+++ Assets/_Project/Scripts/Testing.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Shapes;
 
 public class Testing : MonoBehaviour
 {
