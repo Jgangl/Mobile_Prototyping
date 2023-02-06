8a203c30081d64f674615da8e2c04130a20dfc62
Jgangl <joshua.gangl@gmail.com>
2023-02-05 22:05:56 -0500

    Updating stuff

--- Assets/_Project/Scripts/PlayerController.cs
+++ Assets/_Project/Scripts/PlayerController.cs
@@ -18,7 +18,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float maxSwipeLength     = 300f;
     
     GameObject previousPlatform;
-    bool canCollideWithPreviousPlatform = true;
+    bool platformCollisionTimeout = true;
     bool mouseHeldDown;
     Rigidbody2D rb;
     BasicTrajectory basicTrajectory;
@@ -31,6 +31,7 @@ public class PlayerController : MonoBehaviour
     Vector2 prevPositionTwo;
     Vector2 currentSwipeForce;
     float flightTime;
+    float hitSpeedThreshold = 1f;
     
     Dictionary<Transform, Vector2> bonePositions;
     List<Bone_Softbody> bones;
@@ -161,7 +162,7 @@ public class PlayerController : MonoBehaviour
         rb.isKinematic = false;
         canMove = false;
 
-        SetVelocity(Vector2.zero);
+        //SetVelocity(Vector2.zero);
         
         DisableMovingJoint();
 
@@ -178,9 +179,9 @@ public class PlayerController : MonoBehaviour
 
     IEnumerator IgnorePlatformTimer() 
     {
-        canCollideWithPreviousPlatform = false;
+        platformCollisionTimeout = false;
         yield return new WaitForSeconds(platformIgnoreTime);
-        canCollideWithPreviousPlatform = true;
+        platformCollisionTimeout = true;
     }
 
     public Vector2 GetObjectAverageVelocity() 
@@ -208,7 +209,15 @@ public class PlayerController : MonoBehaviour
         if (stuckToPlatform)
             return;
 
-        bool bHitPlatform = collision.gameObject.TryGetComponent(out Platform platformHit);
+        Platform platformHit;
+        bool bHitPlatform = collision.gameObject.TryGetComponent(out platformHit);
+        if (!bHitPlatform)
+        {
+            bHitPlatform = collision.transform.parent.gameObject.TryGetComponent(out platformHit);
+        }
+        
+        bool bHitPlatformIsCurrent = CurrentPlatform == platformHit;
+        
 /*
         float velocityMagnitude = GetObjectAverageVelocity().magnitude;
         if (velocityMagnitude > 1f && bonesCanCollide) 
@@ -223,12 +232,36 @@ public class PlayerController : MonoBehaviour
             SpawnHitParticles(collision);
         }
 */
+
+        bool speedThresholdPassed = GetObjectAverageVelocity().magnitude > hitSpeedThreshold;
+
+        // If hit a platform OR didn't hit a platform but going fast enough
+        if (bHitPlatform || speedThresholdPassed)
+        {
+            AudioManager.Instance.PlaySquishSound();
+            CinemachineShake.Instance.ShakeCamera(0.5f, 0.2f);
+            
+            slimeGenerator.Generate(collision.contacts[0].point, collision.gameObject);
+            
+            StartCoroutine("IgnoreBonesTimer");
+
+            SpawnHitParticles(collision);
+        }
+
         if (!bHitPlatform)
             return;
         
-        if (platformHit is MovingPlatform && canCollideWithPreviousPlatform && !stuckToPlatform)
+        Debug.Log("platformCollisionTimeout: " + platformCollisionTimeout);
+        Debug.Log("bHitPlatformIsCurrent: " + bHitPlatformIsCurrent);
+
+        // Platform collision hasn't timed out and hit platform is same as current
+        if (!platformCollisionTimeout && bHitPlatformIsCurrent)
+            return;
+        
+        CurrentPlatform = platformHit;
+
+        if (platformHit is MovingPlatform)
         {
-            CurrentPlatform = platformHit;
             StopMovement(false);
             EnableMovingJoint(platformHit.GetComponent<Rigidbody2D>());
 
@@ -239,8 +272,9 @@ public class PlayerController : MonoBehaviour
 
             stuckToPlatform = true;
         }
-        else if (bHitPlatform && canCollideWithPreviousPlatform && !stuckToPlatform) 
+        else
         {
+            Debug.Log("Stopping movement in else");
             StopMovement(true);
 
             if (boneCollisionDict.ContainsKey(bone))
@@ -252,38 +286,27 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void SpawnHitParticles(Collision2D collision)
-    {
-        //  CALCULATE PARTICLES ROTATION USING DIRECTION OF TRAVEL
-        Vector2 currentPosition = transform.position;
-        Vector2 dirOfTravel = (currentPosition - prevPositionTwo).normalized;
-
-        // Find angle between x axis and direction of travel
-        float angle = Mathf.Atan2(dirOfTravel.y, dirOfTravel.x) * Mathf.Rad2Deg;
-
-        // Rotating the angle around an axis (Similar to applying the rotation to a specfic axis)
-        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-
-        Vector2 colPoint = collision.contacts[0].point;
-
-        // Create particles and apply rotation
-        GameObject particles = Instantiate(hitParticles, colPoint, Quaternion.identity);
-        particles.transform.rotation = q;
-        Destroy(particles, 0.5f);
-    }
-
     public void OnChildCollisionStay2D(Bone_Softbody bone, Collision2D collision) 
     {
-        if (collision.gameObject.CompareTag("Platform"))
+        // Fixes sliding across platform
+        if (collision.gameObject.CompareTag("Player") || isDead)
+            return;
+
+        if (stuckToPlatform)
+            return;
+
+        bool bHitPlatform = collision.gameObject.TryGetComponent(out Platform platformHit);
+        bool bHitPlatformIsCurrent = CurrentPlatform == platformHit;
+
+        //Debug.Log(CurrentPlatform);
+        //Debug.Log(CurrentPlatform);
+        
+        // If timer timed out and hit the same platform 
+        if (bonesCanCollide && bHitPlatformIsCurrent )
         {
-            if (stuckToPlatform)
-                return;
-            
-            if (bonesCanCollide)
-            {
-                StopMovement(true);
-                stuckToPlatform = true;
-            }
+            Debug.Log("OnCollisionStay Stopping movement");
+            StopMovement(true);
+            stuckToPlatform = true;
         }
     }
 
@@ -314,6 +337,26 @@ public class PlayerController : MonoBehaviour
                 stuckToPlatform = false;
             }
         }
+    }
+    
+    void SpawnHitParticles(Collision2D collision)
+    {
+        //  CALCULATE PARTICLES ROTATION USING DIRECTION OF TRAVEL
+        Vector2 currentPosition = transform.position;
+        Vector2 dirOfTravel = (currentPosition - prevPositionTwo).normalized;
+
+        // Find angle between x axis and direction of travel
+        float angle = Mathf.Atan2(dirOfTravel.y, dirOfTravel.x) * Mathf.Rad2Deg;
+
+        // Rotating the angle around an axis (Similar to applying the rotation to a specfic axis)
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        Vector2 colPoint = collision.contacts[0].point;
+
+        // Create particles and apply rotation
+        GameObject particles = Instantiate(hitParticles, colPoint, Quaternion.identity);
+        particles.transform.rotation = q;
+        Destroy(particles, 0.5f);
     }
 
     IEnumerator IgnoreBonesTimer() 
